8fbd7596946881aadb462c0ed129f8cf61419fdf
emilieDelsol <emiliedelsol1@gmail.com>
2020-10-18 21:08:10 +0200

    black theme change avatar red and classic need add fonctional

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,10 +5,14 @@ img.addEventListener("click", function(){
     {
         img.src="image/avatarEmilieBgWhite.jpg";
     }
-    else 
+    else
     {
         img.src="image/avatarEmilieBgWhiteRed.jpg";
     }
+
+    
+
+    
 });
 
 
@@ -40,16 +44,45 @@ img.addEventListener("click", function(){
     
     }
 function darkFunction () {
+    //change Bg yellow by background image code
     var allYellowBg=document.querySelectorAll(".yellow-bg");
-    for (i=0 ; i<allYellowBg.length;i++){
+    for (i=0 ; i<allYellowBg.length;i++){ 
         allYellowBg[i].classList.toggle("darkTheme");
     }
-    document.querySelector("body").classList.toggle("darkThemeBody");
+
+    //Change body background
+    document.querySelector("body").classList.toggle("darkThemeBody"); 
     
+    //change text color in white
     var darkText=document.querySelectorAll(".darkText");
-    for (j=0;j<darkText.length;j++){
+    for (j=0;j<darkText.length;j++){ 
         darkText[i].classList.toggle("darkThemeText");
     }
-        
-}
+
+    //change desktop img opacity
+    document.getElementById("desktop").style.opacity="0.8";
+
+    var column = document.getElementsByClassName("column");
+    for (n=0; n<column.length; n++){
+        column[n].style.backgroundColor="rgba(233, 240, 243, 0.8)";
+    }
+    if (img.getAttribute('src')=="image/avatarEmilieBgWhiteRed.jpg")
+    {
+        img.src="image/avatarEmilieBgBlackRed.jpg"
+    }
+    else if (img.getAttribute('src')=="image/avatarEmilieBgWhite.jpg")
+    {
+        img.src="image/avatarEmilieBgBlack.jpg";
+    }
+    else if (img.getAttribute('src')=="image/avatarEmilieBgBlackRed.jpg")
+    {
+        img.src="image/avatarEmilieBgWhiteRed.jpg";
+    }
+    else
+    {
+        img.src="image/avatarEmilieBgWhite.jpg";
+    }
+
     
+    
+}
